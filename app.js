5eed476b79b4f5554fc1fe4e814ea64ad6f6c10e
ParkHyunSoo <parkhyuns00@naver.com>
2023-07-12 16:22:30 +0900

    feat: Socket & DB connection

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const socketConnect = require('./socket.js');
+const dotenv = require('dotenv');
+const db = require('./config/db.js');
 
-require('dotenv').config({path : './.env'});
+dotenv.config();
 app.use('/public', express.static('public'));
 
+// 서버 가동
 const server = app.listen(process.env.PORT, () => {
     console.log('Listening on ' + process.env.PORT);
-});
-const socketConnect = require('./socket.js');
 
+    // Node - Mysql DB 연결 테스트
+    db.query('select * from test', (err, result, fields) => {
+        if (err){
+            console.log(err);
+        } else {
+            console.log(result[0].id + '  ' + result[0].pwd);
+        }
+    });
+});
+
+// 소켓 연결
 socketConnect(server, app);
--- socket.js
+++ socket.js
@@ -1,20 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const socketIO = require('socket.io');
 
 module.exports = (server, app) =>{
     const http = require('http').createServer(app);
-    const {Server} = require('socket.io');
-    const io = new Server(http);
+    const io = socketIO(server, {path:'/socket.io'}, {maxHttpBufferSizke:1e8});
 
+    // 소켓 연결
     io.on('connection', (socket) => {
-        console.log('Client Connected');
+        console.log("user connect : " + socket.id);
 
-        socket.on('data', (data) => {
+        // 클라이언트에서 obj 파일 달라는 이벤트 수신시
+        socket.on('connectReceive', (data) => {
             console.log(data);
 
-            socket.emit('data', 'Send');
+            const filePath = path.join(__dirname, 'public', 'mesh.obj');
+            console.log(filePath);
+
+            fs.readFile(filePath, (err, fileContent) => {
+              if (err){
+                console.log(err);
+              } else {
+                let base64File = fileContent.toString('base64');
+
+                socket.emit("testFile", {success: true, base64File});
+              }
+            })            
         });
 
+        // 소켓 연결 해제시
         socket.on('disconnect', () => {
-            console.log('Disconnected');
-        });
+            console.log("user disconnected");
+        })
+
     });
 }
