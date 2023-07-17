88ba288ce319d4ae46b75629b9f898e166c10116
ParkHyunSoo <parkhyuns00@naver.com>
2023-07-17 16:51:11 +0900

    feat: Receive File & LoginSystem

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const socketConnect = require('./socket.js');
 const dotenv = require('dotenv');
 const db = require('./config/db.js');
+const loginSystem = require('./services/loginSystem.js');
 
 dotenv.config();
 app.use('/public', express.static('public'));
@@ -10,15 +11,6 @@ app.use('/public', express.static('public'));
 // 서버 가동
 const server = app.listen(process.env.PORT, () => {
     console.log('Listening on ' + process.env.PORT);
-
-    // Node - Mysql DB 연결 테스트
-    db.query('select * from test', (err, result, fields) => {
-        if (err){
-            console.log(err);
-        } else {
-            console.log(result[0].id + '  ' + result[0].pwd);
-        }
-    });
 });
 
 // 소켓 연결
--- socket.js
+++ socket.js
@@ -2,37 +2,68 @@ const fs = require('fs');
 const path = require('path');
 const socketIO = require('socket.io');
 
+
 module.exports = (server, app) =>{
     const http = require('http').createServer(app);
     const io = socketIO(server, {path:'/socket.io'}, {maxHttpBufferSizke:1e8});
+    let data = "";
 
     // 소켓 연결
     io.on('connection', (socket) => {
-        console.log("user connect : " + socket.id);
+        console.log("User connect : " + socket.id);
+
+        io.to(socket.id).emit('test', '...');
 
         // 클라이언트에서 obj 파일 달라는 이벤트 수신시
-        socket.on('connectReceive', (data) => {
-            console.log(data);
+        // socket.on('connectReceive', (data) => {
+        //     console.log(data);
 
-            const filePath = path.join(__dirname, 'public', 'mesh.obj');
-            console.log(filePath);
+        //     const filePath = path.join(__dirname, 'public', 'mesh.obj');
+        //     console.log(filePath);
 
-            fs.readFile(filePath, (err, fileContent) => {
+        //     fs.readFile(filePath, (err, fileContent) => {
+        //       if (err){
+        //         console.log(err);
+        //       } else {
+
+        //         // Base64 파일 인코딩
+        //         let base64File = fileContent.toString('base64');
+        //         socket.emit("testFile", {success: true, base64File});
+        //       }
+        //     })            
+        // });
+
+        /* 클라이언트에서 Base64 인코딩된 파일 분할 전송
+         * 수신하여 합친 후 디코딩해서 저장
+        */
+        socket.on('sendFile', (encodedData) => {
+          const total = encodedData.total;
+          const count = encodedData.count;
+          
+          if (encodedData.count === encodedData.total){
+            data += encodedData.data;
+            console.log(`Done.  Length : ${data.length}`);
+
+            const decodedFile = Buffer.from(data, 'base64');
+            const savePath = path.join(__dirname, 'public', 'receive.obj');
+            fs.writeFile(savePath, decodedFile, (err) => {
               if (err){
-                console.log(err);
+                console.log('Err : ' + err);
               } else {
-
-                // Base64 파일 인코딩
-                let base64File = fileContent.toString('base64');
-                socket.emit("testFile", {success: true, base64File});
+                console.log('Save Complete : ' + savePath);
               }
-            })            
+            });
+
+          } else{
+            console.log(`${(count / total * 100.0).toFixed(2)}% Downloaded...`);
+            data += encodedData.data;
+          }
         });
 
         // 소켓 연결 해제시
         socket.on('disconnect', () => {
             console.log("user disconnected");
+            data = "";
         })
-
     });
 }
